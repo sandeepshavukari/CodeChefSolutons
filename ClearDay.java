b3c2c29f4325e88b93fcf7b42cccd320627da7e0
sandeepshavukari <115909841+sandeepshavukari@users.noreply.github.com>
2024-08-29 15:31:22 +0530

    ClearDay.java

---
+++ ClearDay.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s= new Scanner(System.in);
+		int a= s.nextInt();
+		int b= s.nextInt();
+		int c= 7-a-b;
+		System.out.println(c);
+
+	}
+}
