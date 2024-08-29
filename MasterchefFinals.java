ff9fe4041d2209469d2aa7172d21f9a2be52526f
sandeepshavukari <115909841+sandeepshavukari@users.noreply.github.com>
2024-08-29 15:54:54 +0530

    MasterchefFinals.java
    Given that 10 participants if chef raank is not less than or equal to 10 he cant able to win so that if x be the rank of chef Then x> 10 means the rank of chef is grater than 10 so that ge cant able to win if not he can win

---
+++ MasterchefFinals.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s= new Scanner(System.in);
+		{
+		    int t=s.nextInt();
+		    while(t-->0)
+		    {
+		        int x=s.nextInt();
+		        if(x>10)
+		        System.out.println("NO");
+		        else
+		        System.out.println("YES");
+		    }
+		}
+
+	}
+}
