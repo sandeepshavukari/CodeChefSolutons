d4c11af9ffe8aa74a30586c3bb586c4a9cddcbe3
sandeepshavukari <115909841+sandeepshavukari@users.noreply.github.com>
2024-08-29 15:30:21 +0530

    LuckySeven.java

---
+++ LuckySeven.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    String s;
+	    char c;
+	    Scanner sc= new Scanner(System.in);
+	    s=sc.next();
+	    char a[]=s.toCharArray();
+	   // for(int i=0;i<str.length;i++)
+	    c=a[6];
+	    System.out.println(c);
+		// your code goes here
+
+	}
+}
