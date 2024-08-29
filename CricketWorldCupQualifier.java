f9683d471787d9bbf505162c986f80936746085e
sandeepshavukari <115909841+sandeepshavukari@users.noreply.github.com>
2024-08-29 15:28:49 +0530

    CricketWorldCupQualifier.java

---
+++ CricketWorldCupQualifier.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner s= new Scanner(System.in);
+        int t=s.nextInt();
+        if(t>=12)
+        System.out.println("Yes");
+        else
+        System.out.println("No");
+	}
+}
