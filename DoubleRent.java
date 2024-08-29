b7c8ed459679331e544cf8458606b33c7600fd2e
sandeepshavukari <115909841+sandeepshavukari@users.noreply.github.com>
2024-08-29 15:49:49 +0530

    DoubleRent.java

---
+++ DoubleRent.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s= new Scanner(System.in);
+		int a=s.nextInt();
+		int c= a*2;
+		System.out.println(c);
+
+	}
+}
