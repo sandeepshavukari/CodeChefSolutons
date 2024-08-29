9655d5f5df9003990c04a16938d99b3e26444a38
sandeepshavukari <115909841+sandeepshavukari@users.noreply.github.com>
2024-08-29 15:51:00 +0530

    SavingTaxes.java

---
+++ SavingTaxes.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s= new Scanner(System.in);
+		int t=s.nextInt();
+		while(t-->0)
+		{
+		    int a=s.nextInt();
+		    int b=s.nextInt();
+		    System.out.println(a-b);
+		}
+
+	}
+}
